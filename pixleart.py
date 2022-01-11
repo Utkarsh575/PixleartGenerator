bf07d77ea18ce8616e02b430d17823c421394b71
Utkarsh575 <35625228+Utkarsh575@users.noreply.github.com>
2022-01-11 15:46:22 +0530

    Update pixleart.py

--- pixleart.py
+++ pixleart.py
@@ -28,37 +28,23 @@ def colorClustering(idx, img, k):
             imgC[r][c] = clusterAverages[idx[r][c]]
             
     return imgC
+
 def segmentImgClrRGB(img, k):
-    
     imgC = np.copy(img)
-    
     h = img.shape[0]
     w = img.shape[1]
-    
     imgC.shape = (img.shape[0] * img.shape[1], 3)
-    
-    #5. Run k-means on the vectorized responses X to get a vector of labels (the clusters); 
-    #  
     kmeans = KMeans(n_clusters=k, random_state=0).fit(imgC).labels_
-    
-    #6. Reshape the label results of k-means so that it has the same size as the input image
-    #   Return the label image which we call idx
     kmeans.shape = (h, w)
-
     return kmeans
 
 def kMeansImage(image, k):
     idx = segmentImgClrRGB(image, k)
     return colorClustering(idx, image, k)
 
-
 def pixelate(img, w, h):
     height, width = img.shape[:2]
-
-    # Resize input to "pixelated" size
     temp = cv2.resize(img, (w, h), interpolation=cv2.INTER_LINEAR)
-
-    # Initialize output image
     return cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
 
 img16 =(pixelate(img, 64, 64)) 
